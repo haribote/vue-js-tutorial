186964d506c027fa36ddfae243d499ecfa5319e8
KIMURA Tetsuro <haribote.nobody@gmail.com>
2015-11-05 11:48:17 +0900

    render a list

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -5,9 +5,38 @@
 /** @require vue */
 import Vue from 'vue';
 
+/**
+ * Hello, world
+ */
 new Vue({
-  el: '#app',
+  el: '#app-1',
   data: {
     message: 'Hello, world.'
+  }
+});
+
+/**
+ * Two-way binding
+ */
+new Vue({
+  el: '#app-2',
+  data: {
+    message: 'Hello, world.'
+  }
+});
+
+/**
+ * Render a list
+ */
+new Vue({
+  el: '#app-3',
+  data: {
+    todos: [{
+      text: 'Learn JavaScript'
+    }, {
+      text: 'Learn Vue.js'
+    }, {
+      text: 'Build Something Awesome'
+    }]
   }
 });
